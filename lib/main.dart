b364bfb20a901ba826bcdc6112821c66c8ede4c3
Mujtaba <72826134+mujtabafromthedarkside@users.noreply.github.com>
2023-07-01 23:32:57 +0500

    basic homepage done

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Elevated Game',
-      home: homepage(),
+      home: _MyApp(),
     );
   }
 }
 
-class homepage extends StatelessWidget {
-  const homepage({super.key});
+class _MyApp extends StatelessWidget {
+  const _MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const HomePage();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Elevated Game"),
+        ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) =>
+                    const GamePage(),
+              ),
+            );
+          },
+          child: const Text("Play")
+        )
+      )
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  const GamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Elevated Game"),
+        ),
+    );
   }
 }
