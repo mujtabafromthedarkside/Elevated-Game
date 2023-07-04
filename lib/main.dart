b929e0ae1400b406ef8a6484e85fbb8fbbfcd7ce
Mujtaba <72826134+mujtabafromthedarkside@users.noreply.github.com>
2023-07-05 01:59:20 +0500

    added horizontal motion based on phone tilt
    sensors dependency was included for accelerometer readings.
    
    subscription line was added to avoid printing infinite accelerometer print statements in console. It was a pain in the ass.
    
    I have set the gravity, vY and vX values that seem to work well. These may need to be changed when obstacles are added.
    
    renamed variables to lower camel case.
    
    accelerometer button and page were added for testing. will be removed later on.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,15 +40,29 @@ class HomePage extends StatelessWidget {
         //   title: const Text("Elevated Game"),
         // ),
         body: Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => const GamePage(),
-                    ),
-                  );
-                },
-                child: const Text("Play"))));
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => const GamePage(),
+                        ),
+                      );
+                    },
+                    child: const Text("Play")),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => const TestAccelerometer(),
+                        ),
+                      );
+                    },
+                    child: const Text("Accelerometer")),
+              ],
+            )));
   }
 }
 
@@ -59,23 +77,28 @@ class Square extends StatefulWidget {
 
 class _SquareState extends State<Square> with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  double center_x = 0;
-  double center_y = 0;
-  double velocity = 0;
-  double tapVelocity = -5;
-  double gravity = 0.1;
+  double _x = 0.0;                        //accelerometer x value
+  double _xMax = 0.6;                     //this value results in maxVelocityX
+  double velocityX = 0;
+  double maxVelocityX = -0.4;
+
+  double centerX = 0;                    //square center x coordinate
+  double centerY = 0;                    //square center y coordinate
+  
+  double maxVelocity = -7;
+  double velocityY = 0;
+  double gravity = 0.25;
+
   late double screenWidth;
   late double screenHeight;
   bool gameStart = true;
+  StreamSubscription<AccelerometerEvent>? _subscription;
+
 
   @override
   void initState() {
     super.initState();
 
-    // _animationController = AnimationController(
-    //   vsync: this
-    //   duration: const Duration(milliseconds: 16),
-    // );
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(days: 1),
@@ -85,16 +108,24 @@ class _SquareState extends State<Square> with SingleTickerProviderStateMixin {
     _animationController.addListener(
       () {
         // JUST FOR TESTING ONCE GAME IS COMPLETED, TURN + TO -
-        if ((center_y + widget.size / 2) > screenHeight) {
+        if ((centerY + widget.size / 2) > screenHeight) {
           _animationController.stop();
         }
       },
     );
+
+    _subscription = accelerometerEvents.listen((AccelerometerEvent event) {
+      // print(event);
+      setState(() {
+        _x = event.x;
+      });
+    });
     // _animationController.forward();
   }
 
   @override
   void dispose() {
+    _subscription?.cancel();
     _animationController.dispose();
     super.dispose();
   }
@@ -111,12 +142,17 @@ class _SquareState extends State<Square> with SingleTickerProviderStateMixin {
     // print('updating position');
     // if (velocity <= 0){
       setState((){
-        center_y += velocity;
-        velocity += gravity;
-        if(center_y < 0){
-          center_y = screenHeight;
+        velocityX = _x/_xMax * maxVelocityX;
+        centerX += velocityX;
+        centerX = min(screenWidth - widget.size, centerX);
+        centerX = max(0, centerX);
+
+        centerY += velocityY;
+        velocityY += gravity;
+        if(centerY < 0){
+          centerY = screenHeight;
         }
-        // print("updating position to $center_y");
+        // print("updating position to $centerY");
       // velocity += gravity;
       });
     // }
@@ -127,7 +163,9 @@ class _SquareState extends State<Square> with SingleTickerProviderStateMixin {
     // if (!_animationController.isAnimating) {
       setState(() {
         // Move the square up by a certain amount
-        velocity = tapVelocity;
+        // velocityX = _x/_xMax * maxVelocityX;
+        // velocityY = sqrt(maxVelocity;
+        velocityY = maxVelocity;
       });
       startAnimation();
     // }
@@ -145,17 +183,17 @@ class _SquareState extends State<Square> with SingleTickerProviderStateMixin {
       screenHeight -= padding.top;
 
       //square center
-      center_y = screenHeight/2;//- widget.size;
-      center_x = (screenWidth - widget.size)/2;
-      print("x: $center_x, y: $center_y");
+      centerY = screenHeight/2;//- widget.size;
+      centerX = (screenWidth - widget.size)/2;
+      print("x: $centerX, y: $centerY");
 
-      // print(center_y);
+      // print(centerY);
       print("build started");
       gameStart = false;
     }
 
     return Transform.translate(
-      offset: Offset(center_x, center_y),
+      offset: Offset(centerX, centerY),
       child: GestureDetector(
           behavior: HitTestBehavior.opaque,
           onTap: tapFun,
@@ -194,4 +232,60 @@ class _GamePageState extends State<GamePage> {
       body: Square(size: squareSize),
     );
   }
+}
+
+class TestAccelerometer extends StatefulWidget {
+  const TestAccelerometer({super.key});
+
+  @override
+  State<TestAccelerometer> createState() => _TestAccelerometerState();
+}
+
+class _TestAccelerometerState extends State<TestAccelerometer> {
+  double _x = 0.0;
+  double _y = 0.0;
+  double _z = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      // print(event);
+      setState(() {
+        _x = event.x;
+        _y = event.y;
+        _z = event.z;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the accelerometer values (_x, _y, _z) to determine the angle or tilt
+    // of the phone and update your game logic accordingly.
+    // For example, you can check the value of _x to determine if the phone is
+    // leaning to the right or left.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Phone Angle Detection'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('X: $_x'),
+            Text('Y: $_y'),
+            Text('Z: $_z'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // @override
+  // void dispose() {
+  //   super.dispose();
+  //   // Remember to cancel the accelerometer listener when the widget is disposed
+  //   accelerometerEvents.cancel();
+  // }
 }
